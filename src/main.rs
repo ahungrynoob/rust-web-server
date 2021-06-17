70cfcc289434a7b802e2bac1a37c66a882f2aaf0
丁徐达 <xuda.ding@alibaba-inc.com>
2021-06-18 00:24:07 +0800

    feat: multiple thread

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -21,10 +22,20 @@ fn handle_client(mut stream: TcpStream) {
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
 
     // accept connections and process them serially
     for stream in listener.incoming() {
-        handle_client(stream?);
+        let stream = stream.unwrap();
+        let handle = thread::spawn(move || {
+            handle_client(stream);
+        });
+        thread_vec.push(handle);
     }
+
+    for handle in thread_vec {
+        handle.join().unwrap()
+    }
+
     Ok(())
 }
