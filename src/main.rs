1e006eb51acc12b9e4ff5134f77c0a92bcae3c3f
丁徐达 <xuda.ding@alibaba-inc.com>
2021-06-19 15:44:12 +0800

    feat: use thread pool

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use mylib::ThreadPool;
 use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
 
 fn handle_client(mut stream: TcpStream) {
     let mut buffer = [0; 512];
@@ -22,19 +22,12 @@ fn handle_client(mut stream: TcpStream) {
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080")?;
-    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+    let pool = ThreadPool::new(4);
 
     // accept connections and process them serially
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let handle = thread::spawn(move || {
-            handle_client(stream);
-        });
-        thread_vec.push(handle);
-    }
-
-    for handle in thread_vec {
-        handle.join().unwrap()
+        pool.excute(|| handle_client(stream));
     }
 
     Ok(())
